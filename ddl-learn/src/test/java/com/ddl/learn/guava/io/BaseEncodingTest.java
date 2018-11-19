d390951afe069b5361703ef01b4ffd61b09e78a3
dongdongliu <489582986@qq.com>
2018-11-19 17:55:06 +0800

    guava之base64

--- ddl-learn/src/test/java/com/ddl/learn/guava/io/BaseEncodingTest.java
+++ ddl-learn/src/test/java/com/ddl/learn/guava/io/BaseEncodingTest.java
@@ -26,7 +26,7 @@ public class BaseEncodingTest {
         System.out.println(Base64.encode("hello"));
         assertThat(Base64.encode("hello"), equalTo(BaseEncoding.base64().encode("hello".getBytes())));
         assertThat(Base64.encode("alex"), equalTo(BaseEncoding.base64().encode("alex".getBytes())));
-        assertThat(Base64.encode("wangwenjun"), equalTo(BaseEncoding.base64().encode("wangwenjun".getBytes())));
+        assertThat(Base64.encode("hadoop"), equalTo(BaseEncoding.base64().encode("hadoop".getBytes())));
         assertThat(Base64.encode("scala"), equalTo(BaseEncoding.base64().encode("scala".getBytes())));
         assertThat(Base64.encode("kafka"), equalTo(BaseEncoding.base64().encode("kafka".getBytes())));
     }
