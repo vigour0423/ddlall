7e3af58e6ce787ee7997b841a724c715d6d24332
dongdongliu <489582986@qq.com>
2018-11-20 09:57:54 +0800

    guava 事件总线

--- ddl-learn/src/main/java/com/ddl/learn/guava/eventbus/ComEachOtherEventBusExample.java
+++ ddl-learn/src/main/java/com/ddl/learn/guava/eventbus/ComEachOtherEventBusExample.java
@@ -4,16 +4,9 @@ import com.google.common.eventbus.EventBus;
 import com.ddl.learn.guava.eventbus.service.QueryService;
 import com.ddl.learn.guava.eventbus.service.RequestQueryHandler;
 
-/***************************************
- * @author:Alex Wang
- * @Date:2017/10/19
- * 532500648
- ***************************************/
-public class ComEachOtherEventBusExample
-{
+public class ComEachOtherEventBusExample {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         final EventBus eventBus = new EventBus();
         QueryService queryService = new QueryService(eventBus);
         eventBus.register(new RequestQueryHandler(eventBus));
--- ddl-learn/src/main/java/com/ddl/learn/guava/eventbus/ExceptionEventBusExample.java
+++ ddl-learn/src/main/java/com/ddl/learn/guava/eventbus/ExceptionEventBusExample.java
@@ -3,15 +3,16 @@ package com.ddl.learn.guava.eventbus;
 import com.google.common.eventbus.EventBus;
 import com.ddl.learn.guava.eventbus.listeners.ExceptionListener;
 
-/***************************************
- * @author:Alex Wang
- * @Date:2017/10/19
- * 532500648
- ***************************************/
-public class ExceptionEventBusExample
-{
-    public static void main(String[] args)
-    {
+
+public class ExceptionEventBusExample {
+    public static void main(String[] args) {
+
+        final EventBus eventBus1 = new EventBus();
+        eventBus1.register(new ExceptionListener());
+
+        eventBus1.post("exception post");
+
+
         final EventBus eventBus = new EventBus((exception, context) ->
         {
             System.out.println(context.getEvent());
@@ -24,18 +25,6 @@ public class ExceptionEventBusExample
         eventBus.post("exception post");
     }
 
-/*
-    static class ExceptionHandler implements SubscriberExceptionHandler
-    {
 
-        @Override
-        public void handleException(Throwable exception, SubscriberExceptionContext context)
-        {
-            System.out.println(context.getEvent());
-            System.out.println(context.getEventBus());
-            System.out.println(context.getSubscriber());
-            System.out.println(context.getSubscriberMethod());
-        }
-    }*/
 
 }
