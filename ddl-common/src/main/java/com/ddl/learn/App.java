0ae2feb93abea2b96121772d9257e65b2b43498c
dongdongliu <489582986@qq.com>
2019-01-13 14:59:40 +0800

    idea 自动生成的文件

--- ddl-common/src/main/java/com/ddl/learn/App.java
+++ ddl-common/src/main/java/com/ddl/learn/App.java
@@ -1,13 +1,110 @@
 package com.ddl.learn;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.converter.PicturesManager;
+import org.apache.poi.hwpf.converter.WordToHtmlConverter;
+import org.apache.poi.hwpf.usermodel.Picture;
+import org.apache.poi.hwpf.usermodel.PictureType;
+import org.apache.poi.xwpf.converter.core.BasicURIResolver;
+import org.apache.poi.xwpf.converter.core.FileImageExtractor;
+import org.apache.poi.xwpf.converter.xhtml.XHTMLConverter;
+import org.apache.poi.xwpf.converter.xhtml.XHTMLOptions;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
 /**
- * Hello world!
- *
+ * <p>Title:Word2007ToHtml </p>
+ * <p>Company: </p>
+ * @author
+ * @date 2018年4月11日下午2:21:30
+ * Description:
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	@Test
+    public void word2007ToHtml() throws Exception {
+        String filepath = "d:\\";
+        String sourceFileName =filepath+"ddl.docx";
+        String targetFileName = filepath+"1496717486420.html";
+        String imagePathStr = filepath+"/image/";
+        OutputStreamWriter outputStreamWriter = null;
+        try {
+          XWPFDocument document = new XWPFDocument(new FileInputStream(sourceFileName));
+          XHTMLOptions options = XHTMLOptions.create();
+          // 存放图片的文件夹
+          options.setExtractor(new FileImageExtractor(new File(imagePathStr)));
+          // html中图片的路径
+          options.URIResolver(new BasicURIResolver("image"));
+          outputStreamWriter = new OutputStreamWriter(new FileOutputStream(targetFileName), "utf-8");
+          XHTMLConverter xhtmlConverter = (XHTMLConverter) XHTMLConverter.getInstance();
+          xhtmlConverter.convert(document, outputStreamWriter, options);
+        } finally {
+          if (outputStreamWriter != null) {
+            outputStreamWriter.close();
+          }
+        }
+
+      }
+
+    public static void main(String[] args) throws Throwable {
+        final String path = "d:\\";
+        final String file = "contract.doc";
+        InputStream input = new FileInputStream(path + file);
+        HWPFDocument wordDocument = new HWPFDocument(input);
+        WordToHtmlConverter wordToHtmlConverter = new WordToHtmlConverter(
+                DocumentBuilderFactory.newInstance().newDocumentBuilder()
+                        .newDocument());
+        wordToHtmlConverter.setPicturesManager(new PicturesManager() {
+            public String savePicture(byte[] content, PictureType pictureType,
+                                      String suggestedName, float widthInches, float heightInches) {
+                return suggestedName;
+            }
+        });
+        wordToHtmlConverter.processDocument(wordDocument);
+        List pics = wordDocument.getPicturesTable().getAllPictures();
+        if (pics != null) {
+            for (int i = 0; i < pics.size(); i++) {
+                Picture pic = (Picture) pics.get(i);
+                try {
+                    pic.writeImageContent(new FileOutputStream(path
+                            + pic.suggestFullFileName()));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Document htmlDocument = wordToHtmlConverter.getDocument();
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        DOMSource domSource = new DOMSource(htmlDocument);
+        StreamResult streamResult = new StreamResult(outStream);
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer serializer = tf.newTransformer();
+        serializer.setOutputProperty(OutputKeys.ENCODING, "utf-8");
+        serializer.setOutputProperty(OutputKeys.INDENT, "yes");
+        serializer.setOutputProperty(OutputKeys.METHOD, "html");
+        serializer.transform(domSource, streamResult);
+        outStream.close();
+        String content = new String(outStream.toByteArray());
+        FileUtils.writeStringToFile(new File(path, "contract33.html"), content, "utf-8");
     }
+
+
 }
+
