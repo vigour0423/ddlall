9969a834d5ce14b324101d60575bedb22f4087e1
dongdongliu <489582986@qq.com>
2019-02-19 17:49:16 +0800

    玩转数据结构-avl(平衡二叉树)

--- ddl-datastructure/src/main/java/com/ddl/learn/tree/avl/AVLTree.java
+++ ddl-datastructure/src/main/java/com/ddl/learn/tree/avl/AVLTree.java
@@ -1,5 +1,7 @@
 package com.ddl.learn.tree.avl;
 
+import com.ddl.learn.FileOperation;
+
 import java.util.ArrayList;
 
 public class AVLTree<K extends Comparable<K>, V> {
@@ -388,4 +390,37 @@ public class AVLTree<K extends Comparable<K>, V> {
         return retNode;
     }
 
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+
+            AVLTree<String, Integer> map = new AVLTree<>();
+            for (String word : words) {
+                if (map.contains(word))
+                    map.set(word, map.get(word) + 1);
+                else
+                    map.add(word, 1);
+            }
+
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE: " + map.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+
+            System.out.println("is BST : " + map.isBST());
+            System.out.println("is Balanced : " + map.isBalanced());
+
+            for (String word : words) {
+                map.remove(word);
+                if (!map.isBST() || !map.isBalanced()) {
+                    throw new RuntimeException();
+                }
+            }
+        }
+
+        System.out.println();
+    }
 }
