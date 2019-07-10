ea5709b4b861e637ae76150a3deda19e1b2dccfe
dongdong.liu <489582986@qq.com>
2019-07-10 18:25:06 +0800

    dubbo学习

--- ddl-dubbo/boot-order-service-consumer/src/main/java/com/ddl/gmall/service/impl/OrderServiceImpl.java
+++ ddl-dubbo/boot-order-service-consumer/src/main/java/com/ddl/gmall/service/impl/OrderServiceImpl.java
@@ -28,7 +28,6 @@ public class OrderServiceImpl implements OrderService {
     @HystrixCommand(fallbackMethod = "hello")
     @Override
     public List<UserAddress> initOrder(String userId) {
-        // TODO Auto-generated method stub
         System.out.println("用户id：" + userId);
         //1、查询用户的收货地址
         List<UserAddress> addressList = userService.getUserAddressList(userId);
