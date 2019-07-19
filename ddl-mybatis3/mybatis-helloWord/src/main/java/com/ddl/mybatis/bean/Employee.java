33a92ffc9799b58ddf319fcb3a5b06e3246bc792
dongdong.liu <489582986@qq.com>
2019-07-19 18:18:09 +0800

    mybatis3缓存整合

--- ddl-mybatis3/mybatis-helloWord/src/main/java/com/ddl/mybatis/bean/Employee.java
+++ ddl-mybatis3/mybatis-helloWord/src/main/java/com/ddl/mybatis/bean/Employee.java
@@ -2,8 +2,10 @@ package com.ddl.mybatis.bean;
 
 import org.apache.ibatis.type.Alias;
 
+import java.io.Serializable;
+
 @Alias("emp")
-public class Employee {
+public class Employee implements Serializable {
 
     private Integer id;
 
