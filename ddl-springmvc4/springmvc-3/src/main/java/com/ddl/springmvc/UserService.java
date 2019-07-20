55583c6aed3063573be2038ac754f174c95a0b98
dongdong.liu <489582986@qq.com>
2019-07-20 20:48:12 +0800

    mybatis3

--- ddl-springmvc4/springmvc-3/src/main/java/com/ddl/springmvc/UserService.java
+++ ddl-springmvc4/springmvc-3/src/main/java/com/ddl/springmvc/UserService.java
@@ -1,14 +1,14 @@
 package com.ddl.springmvc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
-    @Autowired
-    private HelloWorld helloWorld;
-
+    /**
+     * Spring IOC 容器中的 bean 却不能来引用 SpringMVC IOC 容器中的 bean
+     */
+    /*@Autowired
+    private HelloWorld helloWorld;*/
     public UserService() {
         System.out.println("UserService Constructor...");
     }
